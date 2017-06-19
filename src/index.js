b06d1a7e8d39a9d714f1c8f039dd6b0a4c633640
Eric Cavanna <almightyeric@gmail.com>
2017-06-18 23:11:14 -0700

    Enable use of the "Redux DevTools Extension" Chrome extension

--- src/index.js
+++ src/index.js
@@ -9,7 +9,21 @@ import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
-let appStore = createStore(combinedReducer);
+let appStore = createStore(
+  combinedReducer,
+  //
+  // Instead of defining the app's initial state here, we define it (piece-by-piece) in the individual reducers.
+  //
+  undefined,
+  //
+  // Enable the use of the "Redux DevTools Extension" Chrome extension, if it is installed in the visitor's web browser.
+  // Note: I am enabling it in this direct way---instead of by using the `redux-devtools-extension` npm package--in an
+  // attempt to facilitate disabling it, should I eventually choose to.
+  //
+  // Reference: https://github.com/zalmoxisus/redux-devtools-extension#11-basic-store
+  //
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={appStore}>
